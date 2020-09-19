0174afc82f051125df24685e081779392ede59c8
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2020-09-20 01:00:59 +0700

    Create AliceBobAndChocolate.java

---
+++ AliceBobAndChocolate.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    int[] chocolate = new int[n];
+    for(int i=0; i<n; i++) {
+      chocolate[i] = scan.nextInt();
+    }
+    
+    int aliceTime = 0;
+    int bobTime = 0;
+    int i = 0, j = n-1;
+    
+    while(i<=j) {
+      if(aliceTime + chocolate[i] <= bobTime + chocolate[j]) {
+        aliceTime += chocolate[i];
+        i++;
+      } else {
+        bobTime += chocolate[j];
+        j--;
+      }
+    }
+    System.out.println(i + " " + (n-i));
+  }
+}
