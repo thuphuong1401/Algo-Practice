5f5ac94ecdd9820d8d5a182bc67104265755e2d3
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2020-10-02 02:16:39 +0700

    Create ThrowingCardsAway.java

---
+++ ThrowingCardsAway.java
@@ -0,0 +1,47 @@
+/*
+Throwing cards away - UVA
+*/
+
+import java.util.Scanner;
+import java.util.Queue;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    ArrayList<Integer> input = new ArrayList<>();
+    while(scan.hasNext()) {
+      input.add(scan.nextInt());
+    }
+    
+    for(int n : input) {
+      Queue<Integer> deck = new LinkedList<>();
+      ArrayList<Integer> discarded = new ArrayList<>();
+      
+      if(n == 0) {
+        continue;
+      }
+      
+      for(int j = 1; j <= n; j++) {
+        deck.add(j);
+      }
+      
+      while(deck.size() >= 2) {
+        int discarded1 = deck.poll();
+        discarded.add(discarded1);
+        deck.add(deck.poll());
+      }
+      
+      System.out.print("Discarded cards:");
+      for (int i = 0; i < n - 1; i++) {
+        if (i != 0) {
+          System.out.print(",");
+        }
+        System.out.format(" %d", discarded.get(i));
+      }
+      System.out.println();
+      System.out.format("Remaining card: %d\n", deck.poll());
+    }
+  }
+}
