470294d5ecbc27a9f106cae4a1a5abd065275deb
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2020-11-14 12:09:32 +0700

    Create Transform-The-Expression.java

---
+++ Stack&Queue/Transform-The-Expression.java
@@ -0,0 +1,32 @@
+/*
+https://www.spoj.com/problems/ONP/
+*/
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int numCases = scan.nextInt();
+    Stack<Character> s = new Stack<>();
+    
+    for(int i=0; i<numCases; i++) {
+      char[] expression = scan.next().toCharArray();
+      for(Character symbol : expression) {
+        if(Character.isLetter(symbol)) {
+          System.out.print(symbol);
+        }
+        else if(symbol.equals(')')) {
+         	System.out.print(s.pop());
+        }
+        else if(!symbol.equals('(')) {
+          s.add(symbol);
+        }
+      }
+      System.out.println();
+    }
+  }
+}
+
+
