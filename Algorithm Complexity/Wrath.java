1f8ffc05e1cc58701c58aea44d0ef648af094291
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2020-11-14 11:43:00 +0700

    Create Wrath.java

---
+++ Algorithm Complexity/Wrath.java
@@ -0,0 +1,33 @@
+/*
+https://codeforces.com/problemset/problem/892/B
+*/
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    int[] claw = new int[n];
+    for(int i=0; i<n; i++) {
+      claw[i] = scan.nextInt();
+    }
+    
+    int killed=0; // count number of people killed
+    int j = n-1;
+    int furthestReaching = 0;
+    for(int i = n-1; i >= 0; i--) {
+      j = Math.min(i,j);
+      furthestReaching = Math.max(0, i-claw[i]);
+      
+      // some people killed but not counted yet
+      while(j > furthestReaching) {
+        killed += (j-furthestReaching);
+        j = furthestReaching;
+      }
+      
+    }
+    System.out.println(n-killed);
+    
+  } 
+}
