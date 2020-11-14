d3e98abf97d4d467259af69edb5b686f2945581b
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2020-11-14 11:48:27 +0700

    Create Devu-The-Dumb-Guy.java

---
+++ Sorting/Devu-The-Dumb-Guy.java
@@ -0,0 +1,36 @@
+/*
+https://codeforces.com/problemset/problem/439/B
+*/
+
+import java.util.Collections;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int x = scan.nextInt();
+        ArrayList<Integer> time = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            time.add(scan.nextInt());
+        }
+
+        Collections.sort(time);
+
+        long totalTime = 0;
+        for (int i = 0; i < n; i++) {
+            if (x > 1) {
+                totalTime += 1L * time.get(i) * x--;
+            } else {
+                totalTime += time.get(i);
+            }
+        }
+
+        System.out.println(totalTime);
+
+    }
+}
