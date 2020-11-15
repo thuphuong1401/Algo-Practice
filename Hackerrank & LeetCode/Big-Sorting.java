15ead9efb2080dfcd7c49436510fd7c302a41d96
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2020-11-15 16:10:55 +0700

    Create Big-Sorting.java

---
+++ Hackerrank & LeetCode/Big-Sorting.java
@@ -0,0 +1,57 @@
+/*
+https://www.hackerrank.com/challenges/big-sorting/problem
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the bigSorting function below.
+    static String[] bigSorting(String[] unsorted) {
+        Arrays.sort(unsorted, (x, y) -> x.length() == y.length() ? x.compareTo(y) : Integer.compare(x.length(), y.length()));
+        return unsorted;
+    
+    }
+    
+    
+    
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String[] unsorted = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            String unsortedItem = scanner.next();
+            unsorted[i] = unsortedItem;
+        }
+
+        String[] result = bigSorting(unsorted);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(result[i]);
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
