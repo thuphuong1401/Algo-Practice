ea55dd15c42440203e87f1f689b2e5224bcee0bf
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2020-12-02 19:23:54 +0700

    Create Java-HashSet.java

---
+++ Hackerrank & LeetCode/Java-HashSet.java
@@ -0,0 +1,31 @@
+/*
+https://www.hackerrank.com/challenges/java-hashset/problem
+NOTE: find number of unique element => put into hash set, find the size
+*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+ public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+        
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+
+        HashSet<String> hs = new HashSet<>();
+        for(int i = 0; i < t; i++) {
+            hs.add(pair_left[i] + " " + pair_right[i]);
+            System.out.println(hs.size());
+        }   
+    }
+}
