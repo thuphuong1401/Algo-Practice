81d4b134001fa67aa387794525739b09a6ef53c4
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2020-12-05 19:16:22 +0700

    Update Single-Source-Shortest-Path-Negative-Weights.java

--- Bellman-Ford/Single-Source-Shortest-Path-Negative-Weights.java
+++ Bellman-Ford/Single-Source-Shortest-Path-Negative-Weights.java
@@ -1,8 +1,20 @@
 /*
 https://open.kattis.com/problems/shortestpath3
 Approach: run Bellman-Ford twice.
-1. First run: 
-- Got dist array, 
+Sau khi chạy Bellman-Ford một lần để lấy được trọng số nhỏ nhất từ đỉnh nguồn đến các đỉnh còn lại trong đồ thị. Lúc này xét các khoảng cách thu được:
+
+Nếu khoảng cách từ đỉnh nguồn s tới đỉnh i có giá trị là dương vô cực, nghĩa là không tồn tại đường đi từ s -> i.
+Ngược lại, nếu khoảng cách này khác dương vô cực có thể xảy ra 22 trường hợp:
+Đó là trọng số nhỏ nhất đi từ s -> i.
+Đỉnh ii thuộc chu trình âm nên khảng cách này là không xác định.
+Ta biết rằng thuật toán Bellman-Ford truyền thống chỉ cho phép ta pháp hiện có tồn tại chu trình âm bất kỳ trong đồ thị chứ không chỉ rõ các đỉnh thuộc các chu trình âm trong đồ thị đó. Chạy Bellman-Ford lần hai, nếu lại tìm được một khoảng cách từ s -> v tốt hơn thì chắc chắn v thuộc chu trình âm. Ta gán khoảng cách là âm vô cực.
+
+Cuối cùng với mỗi truy vấn, ta gọi đỉnh yêu cầu uu và so sánh:
+
+Nếu khoảng cách từ s -> u là dương vô cực: in “Impossible”.
+Nếu khoảng cách từ s -> u là âm vô cực: in “-Infinity”.
+Ngược lại, khoảng cách từ s -> u xác định, ta in ra khoảng cách ấy.
+Độ phức tạp: Ta sẽ phải chạy thuật Bellman-Ford 2 lần cho mỗi bộ test tốn O(n * m)O(n∗m) và đọc dữ liệu tốn O(m + q)O(m+q). Vậy độ phức tạp toàn bài là O(T * n * m)O(T∗n∗m) với TT là số lượng bộ test, nn là số đỉnh và mm là số cạnh của đồ thị.
 */
 
 import java.io.*;
