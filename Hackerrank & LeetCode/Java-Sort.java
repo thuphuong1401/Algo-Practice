c2b135fe7ef788d871f40c61d4150f0de0b9a436
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2020-12-07 15:15:25 +0700

    Update Java-Sort.java

--- Hackerrank & LeetCode/Java-Sort.java
+++ Hackerrank & LeetCode/Java-Sort.java
@@ -1,6 +1,10 @@
 /*
 https://www.hackerrank.com/challenges/java-sort/problem
 */
+
+/*
+My version
+*/
 import java.util.*;
 
 class Student{
@@ -70,3 +74,56 @@ class Checker implements Comparator<Student> {
     }
 }
 
+
+/*
+A version in the discussion. Only for Java 8. Uses Comparator.comparing(). <...> thenComparing()
+*/
+import java.util.*;
+
+class Student{
+   private int id;
+   private String fname;
+   private double cgpa;
+   public Student(int id, String fname, double cgpa) {
+      super();
+      this.id = id;
+      this.fname = fname;
+      this.cgpa = cgpa;
+   }
+   public int getId() {
+      return id;
+   }
+   public String getFname() {
+      return fname;
+   }
+   public double getCgpa() {
+      return cgpa;
+   }
+}
+
+//Complete the code
+public class Solution
+{
+   public static void main(String[] args){
+      Scanner in = new Scanner(System.in);
+      int testCases = Integer.parseInt(in.nextLine());
+      
+      List<Student> studentList = new ArrayList<Student>();
+      while(testCases>0){
+         int id = in.nextInt();
+         String fname = in.next();
+         double cgpa = in.nextDouble();
+         
+         Student st = new Student(id, fname, cgpa);
+         studentList.add(st);
+         
+         testCases--;
+      }
+       
+       Collections.sort(studentList,  Comparator.comparing(Student :: getCgpa).reversed().thenComparing(Student :: getFname).thenComparing(Student :: getId));
+      
+         for(Student st: studentList){
+         System.out.println(st.getFname());
+      }
+   }
+}
