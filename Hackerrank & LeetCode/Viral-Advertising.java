900be8fb5355170ee601095e95b727f43a483f02
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2020-12-09 13:24:49 +0700

    Create Viral-Advertising.java

---
+++ Hackerrank & LeetCode/Viral-Advertising.java
@@ -0,0 +1,45 @@
+/*
+https://www.hackerrank.com/challenges/strange-advertising/problem
+*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the viralAdvertising function below.
+    static int viralAdvertising(int n) {
+        int cumulative = 2;
+        int shared = 5;
+        int liked = 5/2;
+        while(n > 1) {
+            shared = liked * 3;
+            liked = shared / 2;
+            cumulative += liked;
+            n--;
+        }
+        return cumulative;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int result = viralAdvertising(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
