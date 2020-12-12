e0061c386cc879152c8c3ffc5aa4d802ca657b81
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2020-12-12 15:15:55 +0700

    Create Find-Digits.java

---
+++ Hackerrank & LeetCode/Find-Digits.java
@@ -0,0 +1,50 @@
+/*
+https://www.hackerrank.com/challenges/find-digits/problem
+*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the findDigits function below.
+    static int findDigits(int n) {
+        int answer = 0;
+        int x = n;
+        while(n > 0) {
+            int digit = n%10;
+            if(digit != 0 && x%digit == 0) {
+                answer++;
+            }
+            n = n/10;
+        }
+        return answer;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int result = findDigits(n);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
