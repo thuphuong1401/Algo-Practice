d00832313a58796e395b65119e253972e8cfc78a
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2020-12-16 11:09:26 +0700

    Create Repeated-String.java

---
+++ Hackerrank & LeetCode/Interview Preparation Kit/Warm-up Challenges/Repeated-String.java
@@ -0,0 +1,51 @@
+/*
+https://www.hackerrank.com/challenges/repeated-string/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the repeatedString function below.
+    static long repeatedString(String s, long n) {
+        long num = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == 'a') {
+                num++;
+            }
+        }
+        long temp = n%(s.length());
+        num = num * (n / s.length());
+        for(int i = 0; i < temp; i++) {
+            if(s.charAt(i) == 'a') {
+                num++;
+            }
+        }
+        return num;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        long n = scanner.nextLong();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long result = repeatedString(s, n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
