6de29e02fa0fa6a8739637c8c1c494d3d41b39a5
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2020-12-17 12:06:08 +0700

    Create Icecream-Parlor.java

---
+++ Hackerrank & LeetCode/Icecream-Parlor.java
@@ -0,0 +1,71 @@
+/*
+https://www.hackerrank.com/challenges/icecream-parlor/problem
+Exactly like 2 sums
+*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the icecreamParlor function below.
+    static int[] icecreamParlor(int m, int[] arr) {
+        int[] answer = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < arr.length; i++) {
+            if(map.containsKey(m - arr[i])) {
+                answer[0] = map.get(m - arr[i]) + 1;
+                answer[1] = i+1;
+            }
+            map.put(arr[i], i);
+        }
+        return answer;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int m = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] arr = new int[n];
+
+            String[] arrItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int arrItem = Integer.parseInt(arrItems[i]);
+                arr[i] = arrItem;
+            }
+
+            int[] result = icecreamParlor(m, arr);
+
+            for (int i = 0; i < result.length; i++) {
+                bufferedWriter.write(String.valueOf(result[i]));
+
+                if (i != result.length - 1) {
+                    bufferedWriter.write(" ");
+                }
+            }
+
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
