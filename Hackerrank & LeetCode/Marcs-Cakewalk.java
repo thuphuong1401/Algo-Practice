b23a0c5ac21f5b8f11e1036aa37bd645494c444d
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2020-12-18 22:47:10 +0700

    Create Marcs-Cakewalk.java

---
+++ Hackerrank & LeetCode/Marcs-Cakewalk.java
@@ -0,0 +1,54 @@
+/*
+https://www.hackerrank.com/challenges/marcs-cakewalk/problem
+*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the marcsCakewalk function below.
+    static long marcsCakewalk(Integer[] calorie) {
+        int n = calorie.length;
+        long minCal = 0;
+        Arrays.sort(calorie, Collections.reverseOrder());
+        for(int i = 0; i < n; i++) {
+            minCal += Math.pow(2, i) * calorie[i];
+        }
+        
+        return minCal;
+        
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        Integer[] calorie = new Integer[n];
+
+        String[] calorieItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            Integer calorieItem = Integer.parseInt(calorieItems[i]);
+            calorie[i] = calorieItem;
+        }
+
+        long result = marcsCakewalk(calorie);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
