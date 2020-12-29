d4b17a9a8a35dcfcf46363a8d0e34d2a3770a2c3
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2020-12-29 14:49:33 +0700

    Update Graph-Connectivity.java

--- Disjoint Set Union (DSU)/Graph-Connectivity.java
+++ Disjoint Set Union (DSU)/Graph-Connectivity.java
@@ -7,67 +7,64 @@ import java.util.*;
 import java.io.*;
 
 class MyCode {
-    
+
     private static int[] parent;
     private static int[] size;
     private static int count;
     private static int n;
-    
-    public static void main (String[] args) {
+
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int numTestCases = scan.nextInt();
         scan.nextLine();
         scan.nextLine();
-        while(numTestCases > 0) {
+        while (numTestCases > 0) {
             char max = scan.nextLine().charAt(0);
             n = max - 'A' + 1;
             makeSet();
             String line;
-            while(scan.hasNextLine() && !(line = scan.nextLine()).equals("")) {
+            while (scan.hasNextLine() && !(line = scan.nextLine()).equals("")) {
                 int x = line.charAt(0) - 'A';
                 int y = line.charAt(1) - 'A';
-                union(x, y);   
+                union(x, y);
             }
             System.out.println(count);
             System.out.println();
-            
+
             numTestCases--;
         }
-	}
-    
-    
+    }
+
     public static void makeSet() {
         count = n;
         parent = new int[n];
         size = new int[n];
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             parent[i] = i;
             size[i] = 1;
         }
     }
-    
-    
+
     public static int find(int p) {
         int root = p;
-        while(root != parent[root]) {
+        while (root != parent[root]) {
             root = parent[root];
         }
-        while(p != root) {
+        while (p != root) {
             int newp = parent[p];
             parent[p] = root;
             p = newp;
         }
         return root;
     }
-    
-    
+
     public static void union(int p, int q) {
         int rootP = find(p);
         int rootQ = find(q);
-        if(rootP == rootQ) {
+        if (rootP == rootQ) {
             return;
         }
-        if(size[rootP] > size[rootQ]) {
+        if (size[rootP] > size[rootQ]) {
             parent[rootQ] = rootP;
             size[rootP] += size[rootQ];
         } else {
@@ -76,7 +73,5 @@ class MyCode {
         }
         count--;
     }
-    
-    
-}
 
+}
