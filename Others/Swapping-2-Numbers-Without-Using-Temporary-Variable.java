f0b98665dead5197edbc803b70995c816dbcc184
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2021-02-07 12:46:56 +0700

    Create Swapping-2-Numbers-Without-Using-Temporary-Variable.java

---
+++ Others/Swapping-2-Numbers-Without-Using-Temporary-Variable.java
@@ -0,0 +1,5 @@
+https://florian.github.io/xor-trick/
+
+https://www.geeksforgeeks.org/swap-two-numbers-without-using-temporary-variable/
+
+General idea: use the cancellation property of XOR (x^x = 0) to turn (x,y) into (y,x)
