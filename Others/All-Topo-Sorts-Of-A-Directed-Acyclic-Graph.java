b8cc64649dc00619dbafc81ada7c16b38edb3b50
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2021-03-02 16:36:09 +0700

    Update All-Topo-Sorts-Of-A-Directed-Acyclic-Graph.java

--- Others/All-Topo-Sorts-Of-A-Directed-Acyclic-Graph.java
+++ Others/All-Topo-Sorts-Of-A-Directed-Acyclic-Graph.java
@@ -5,3 +5,27 @@ Print ALL topological sorted order of a directed acyclic graph
 */
 
 Method used: backtracking
+
+Pseudocode goes something like this: 
+
+    allTopoSort() {
+        calculate array of indegree
+        call util();
+    }
+    
+    
+    util() {    
+        for(vertex v) {
+            if(!visited[v] && indegree[v] == 0) {
+                visited[v] = true;
+                queue.add(v);
+                reduce degree of adjacent vertices. 
+                util();
+                visited[v] = false;
+                add back degree of adjacent vertices
+                queue.remove();
+            }
+        }
+    
+        print queue
+    }
