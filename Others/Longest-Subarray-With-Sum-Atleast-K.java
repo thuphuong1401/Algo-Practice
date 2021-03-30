967ff232948c2c89f5793b89352ab5d7dcf79ac5
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2021-03-30 17:25:23 +0700

    Create Longest-Subarray-With-Sum-Atleast-K.java

---
+++ Others/Longest-Subarray-With-Sum-Atleast-K.java
@@ -0,0 +1,5 @@
+// Longest subarray with sum at least K
+// Notice 2 cases: 
+- All positive integers: might just use sliding windows
+- Positive & negative integers: have to modify a bit
+https://leetcode.com/discuss/interview-question/758045/
