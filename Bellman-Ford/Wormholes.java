b7b7ecab59784c24bc9db1236b4d53c3757babb4
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2021-04-12 17:59:42 +0700

    Update Wormholes.java

--- Bellman-Ford/Wormholes.java
+++ Bellman-Ford/Wormholes.java
@@ -1,3 +1,4 @@
+
 /*
 http://www.cs.nthu.edu.tw/~progcont/ACM/ProblemSetArchive/B_EU_NWRC/1996/nwec96b.html
 */
@@ -8,7 +9,7 @@ class Edge {
     public int source;
     public int target;
     public int weight;
-    
+
     public Edge(int source, int target, int weight) {
         this.source = source;
         this.target = target;
@@ -17,26 +18,26 @@ class Edge {
 }
 
 class MyCode {
-    public static final int MAX = (int)1e9;
+    public static final int MAX = (int) 1e9;
     public static int[] dist;
     public static Edge[] graph;
     public static int[] path;
     public static int star, wormholes;
-    
-	public static void main (String[] args) {
+
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int numTestCase = scan.nextInt();
-        for(int i = 0; i < numTestCase; i++) {
+        for (int i = 0; i < numTestCase; i++) {
             star = scan.nextInt();
             wormholes = scan.nextInt();
             graph = new Edge[wormholes];
             dist = new int[star];
             path = new int[star];
-            for(int j = 0; j < star; j++) {
+            for (int j = 0; j < star; j++) {
                 dist[j] = MAX;
                 path[j] = -1;
             }
-            for(int j = 0; j < wormholes; j++) {
+            for (int j = 0; j < wormholes; j++) {
                 int u = scan.nextInt();
                 int v = scan.nextInt();
                 int w = scan.nextInt();
@@ -44,38 +45,37 @@ class MyCode {
             }
             int s = 0;
             boolean res = BellmanFord(s);
-            if(!res) {
+            if (!res) {
                 System.out.println("possible");
             } else {
                 System.out.println("not possible");
             }
         }
-	}
-    
+    }
+
     public static boolean BellmanFord(int s) {
         int u, v, w;
         dist[s] = 0;
-        for(int i = 1; i <= star-1; i++) {
-            for(int j = 0; j < wormholes; j++) {
+        for (int i = 1; i <= star - 1; i++) {
+            for (int j = 0; j < wormholes; j++) {
                 u = graph[j].source;
                 v = graph[j].target;
                 w = graph[j].weight;
-                if(dist[u] != MAX && dist[u] + w < dist[v]) {
+                if (dist[u] != MAX && dist[u] + w < dist[v]) {
                     dist[v] = dist[u] + w;
                     path[v] = u;
                 }
             }
         }
         // Check for negative cycle
-        for(int i = 0; i < wormholes; i++) {
+        for (int i = 0; i < wormholes; i++) {
             u = graph[i].source;
             v = graph[i].target;
             w = graph[i].weight;
-            if(dist[u] != MAX && dist[u] + w < dist[v]) {
+            if (dist[u] != MAX && dist[u] + w < dist[v]) {
                 return false;
             }
         }
         return true;
     }
 }
-
